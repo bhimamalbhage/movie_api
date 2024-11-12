9ea05245d7021a3effb7b9d09115a6af58eed06d
bhimamalbhage18 <bhimamalbhage@hotmail.com>
2024-11-11 23:59:52 -0600

    connected to mongodb atlas

--- index.js
+++ index.js
@@ -30,8 +30,20 @@ let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
+// mongoose
+//   .connect("mongodb://localhost:27017/movieDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Database connected");
+//   })
+//   .catch((err) => {
+//     console.error("Database connection error:", err);
+//   });
+
 mongoose
-  .connect("mongodb://localhost:27017/movieDB", {
+  .connect(process.env.CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
